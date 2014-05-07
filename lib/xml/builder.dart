3f0a8bb1c51a5e3dd1b3fa5e5be6eae31f7ba01d
Lukas Renggli <renggli@gmail.com>
2014-05-07 23:20:07 +0200

    - slightly simplify the builder

--- lib/xml/builder.dart
+++ lib/xml/builder.dart
@@ -7,34 +7,34 @@ part of xml;
  */
 class XmlBuilder {
 
-  _XmlNodeBuilder _current = new _XmlDocumentBuilder();
+  final List<_XmlNodeBuilder> _stack = new List.from([new _XmlDocumentBuilder()]);
 
   /**
    * Adds a [XmlText] node with the provided `text`.
    */
   void text(String text) {
-    _current.children.add(new XmlText(text));
+    _stack.last.children.add(new XmlText(text));
   }
 
   /**
    * Adds a [XmlCDATA] node with the provided `text`.
    */
   void cdata(String text) {
-    _current.children.add(new XmlCDATA(text));
+    _stack.last.children.add(new XmlCDATA(text));
   }
 
   /**
    * Adds a [XmlProcessing] node with the provided `target` and `text`.
    */
   void processing(String target, String text) {
-    _current.children.add(new XmlProcessing(target, text));
+    _stack.last.children.add(new XmlProcessing(target, text));
   }
 
   /**
    * Adds a [XmlComment] node with the provided `text`.
    */
   void comment(String text) {
-    _current.children.add(new XmlComment(text));
+    _stack.last.children.add(new XmlComment(text));
   }
 
   /**
@@ -49,7 +49,7 @@ class XmlBuilder {
    * If a map of `attributes` is provided the name-value pairs are added to the
    * element declaration, see also [XmlBuilder#attribute].
    *
-   * Finally, `nest` is used to further customize the element and to adds its
+   * Finally, `nest` is used to further customize the element and to add its
    * children. Typically this is a [Function] that defines elements using the
    * same builder object. For convenience `nest` can also be a string or another
    * common object that will be converted to a string and added as a text node.
@@ -59,23 +59,23 @@ class XmlBuilder {
       Map<String, String> namespaces: const {},
       Map<String, String> attributes: const {},
       Object nest: null}) {
-    var builder = new _XmlElementBuilder(_current);
-    _current = builder;
+    var element = new _XmlElementBuilder();
+    _stack.add(element);
     namespaces.forEach(this.namespace);
     attributes.forEach(this.attribute);
     if (nest != null) {
       _insert(nest);
     }
-    builder.name = _buildName(name, namespace);
-    _current = builder.parent;
-    _current.children.add(builder.build());
+    element.name = _buildName(name, namespace);
+    _stack.removeLast();
+    _stack.last.children.add(element.build());
   }
 
   /**
    * Adds a [XmlAttribute] node with the provided `name` and `value`.
    */
   void attribute(String name, String value, {String namespace}) {
-    _current.attributes.add(new XmlAttribute(_buildName(name, namespace), value));
+    _stack.last.attributes.add(new XmlAttribute(_buildName(name, namespace), value));
   }
 
   /**
@@ -87,33 +87,37 @@ class XmlBuilder {
     if (prefix == _XMLNS || prefix == _XML) {
       throw new ArgumentError('The "$prefix" prefix cannot be bound.');
     }
-    if (_current.namespaces.containsValue(prefix)) {
+    if (_stack.last.namespaces.containsValue(prefix)) {
       throw new ArgumentError('The "$prefix" prefix conflicts with existing binding.');
     }
     var name = prefix == null || prefix.isEmpty
         ? new XmlName(_XMLNS)
         : new XmlName(prefix, _XMLNS);
-    _current.attributes.add(new XmlAttribute(name, uri));
-    _current.namespaces[uri] = prefix;
+    _stack.last.attributes.add(new XmlAttribute(name, uri));
+    _stack.last.namespaces[uri] = prefix;
   }
 
   /**
    * Returns the resulting [XmlNode].
    */
-  XmlNode build() => _current.build();
+  XmlNode build() => _stack.last.build();
 
-  /**
-   * Internal method to build a name.
-   */
+  // Internal method to build a name.
   XmlName _buildName(String name, String uri) {
     return uri == null || uri.isEmpty
         ? new XmlName.fromString(name)
-        : new XmlName(name, _current.lookupPrefix(uri));
+        : new XmlName(name, _lookup(uri));
   }
 
-  /**
-   * Internal method to add something to the current element.
-   */
+  // Internal method to lookup an uri prefix.
+  String _lookup(String uri) {
+    var builder = _stack.lastWhere(
+      (builder) => builder.namespaces.containsKey(uri),
+      orElse: () => throw new ArgumentError('Undefined namespace: $uri'));
+    return builder.namespaces[uri];
+  }
+
+  // Internal method to add something to the current element.
   void _insert(Object value) {
     if (value is Function) {
       value();
@@ -131,15 +135,12 @@ abstract class _XmlNodeBuilder {
   List<XmlAttribute> get attributes;
   List<XmlNode> get children;
   XmlNode build();
-  String lookupPrefix(String uri);
 }
 
 class _XmlDocumentBuilder extends _XmlNodeBuilder {
 
   @override
-  Map<String, String> get namespaces {
-    throw new ArgumentError('Unable to define namespaces at the document level.');
-  }
+  final Map<String, String> namespaces = const { _XML_URI: _XML };
 
   @override
   List<XmlAttribute> get attributes {
@@ -151,11 +152,6 @@ class _XmlDocumentBuilder extends _XmlNodeBuilder {
 
   @override
   XmlNode build() => new XmlDocument(children);
-
-  @override
-  String lookupPrefix(String uri) {
-    return uri == _XML_URI ? _XML : throw new ArgumentError('Undefined namespace: $uri');
-  }
 
 }
 
@@ -170,18 +166,9 @@ class _XmlElementBuilder extends _XmlNodeBuilder {
   @override
   final List<XmlNode> children = new List();
 
-  final _XmlNodeBuilder parent;
-
   XmlName name;
-
-  _XmlElementBuilder(this.parent);
 
   @override
   XmlNode build() => new XmlElement(name, attributes, children);
-
-  @override
-  String lookupPrefix(String uri) {
-    return namespaces.containsKey(uri) ? namespaces[uri] : parent.lookupPrefix(uri);
-  }
 
 }
