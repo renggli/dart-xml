8dce1396b408f4262a3fc713fbbec3900c1fec3b
Lukas Renggli <renggli@gmail.com>
2014-09-07 12:29:36 +0200

    Another small optimisation for DOCTYPE parsing, as well as some more constant extraction

--- lib/xml/grammar.dart
+++ lib/xml/grammar.dart
@@ -5,19 +5,29 @@ part of xml;
  */
 class XmlGrammar extends CompositeParser {
 
+  // name patterns
   static const NAME_START_CHARS = ':A-Z_a-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF'
       '\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001\uD7FF'
       '\uF900-\uFDCF\uFDF0-\uFFFD';
   static const NAME_CHARS = '-.0-9\u00B7\u0300-\u036F\u203F-\u2040$NAME_START_CHARS';
+  static const CHAR_DATA = '^<';
 
+  // basic tokens
   static const DOUBLE_QUOTE = '"';
   static const SINGLE_QUOTE = "'";
+  static const EQUALS = '=';
   static const OPEN_COMMENT = '<!--';
   static const CLOSE_COMMENT = '-->';
   static const OPEN_CDATA = '<![CDATA[';
   static const CLOSE_CDATA = ']]>';
+  static const OPEN_ELEMENT = '<';
+  static const CLOSE_ELEMENT = '>';
+  static const OPEN_END_ELEMENT = '</';
+  static const CLOSE_END_ELEMENT = '/>';
   static const OPEN_DOCTYPE = '<!DOCTYPE';
   static const CLOSE_DOCTYPE = '>';
+  static const OPEN_DOCTYPE_BLOCK = '[';
+  static const CLOSE_DOCTYPE_BLOCK = ']';
   static const OPEN_PROCESSING = '<?';
   static const CLOSE_PROCESSING = '?>';
 
@@ -27,7 +37,7 @@ class XmlGrammar extends CompositeParser {
 
     def('attribute', ref('qualified')
       .seq(ref('whitespace').optional())
-      .seq(char('='))
+      .seq(char(EQUALS))
       .seq(ref('whitespace').optional())
       .seq(ref('attributeValue'))
       .permute([0, 4]));
@@ -62,10 +72,10 @@ class XmlGrammar extends CompositeParser {
       .seq(ref('whitespace'))
       .seq(ref('nameToken')
         .or(ref('attributeValue'))
-        .or(char('[').neg().star()
-          .seq(char('['))
-          .seq(char(']').neg().star())
-          .seq(char(']')))
+        .or(any().starLazy(char(OPEN_DOCTYPE_BLOCK))
+          .seq(char(OPEN_DOCTYPE_BLOCK))
+          .seq(any().starLazy(char(CLOSE_DOCTYPE_BLOCK)))
+          .seq(char(CLOSE_DOCTYPE_BLOCK)))
         .separatedBy(ref('whitespace'))
         .flatten())
       .seq(ref('whitespace').optional())
@@ -79,19 +89,19 @@ class XmlGrammar extends CompositeParser {
       .seq(ref('misc'))
       .permute([0, 2, 4])
       .map((list) => list.where((each) => each != null)));
-    def('element', char('<')
+    def('element', char(OPEN_ELEMENT)
       .seq(ref('qualified'))
       .seq(ref('attributes'))
       .seq(ref('whitespace').optional())
-      .seq(string('/>')
-        .or(char('>')
+      .seq(string(CLOSE_END_ELEMENT)
+        .or(char(CLOSE_ELEMENT)
           .seq(ref('content'))
-          .seq(string('</'))
+          .seq(string(OPEN_END_ELEMENT))
           .seq(ref('qualified'))
           .seq(ref('whitespace').optional())
-          .seq(char('>'))))
+          .seq(char(CLOSE_ELEMENT))))
       .map((list) {
-        if (list[4] == '/>') {
+        if (list[4] == CLOSE_END_ELEMENT) {
           return [list[1], list[2], []];
         } else {
           if (list[1] == list[4][3]) {
@@ -110,7 +120,7 @@ class XmlGrammar extends CompositeParser {
       .permute([1, 2]));
     def('qualified', ref('nameToken'));
 
-    def('characterData', pattern('^<').plus().flatten().map(_decodeXml));
+    def('characterData', pattern(CHAR_DATA).plus().flatten().map(_decodeXml));
     def('misc', ref('whitespace')
       .or(ref('comment'))
       .or(ref('processing'))
