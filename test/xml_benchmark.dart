4fb405f3b983f02a1891a577661866c6bd015aee
Lukas Renggli <renggli@gmail.com>
2014-09-21 14:20:02 +0200

    Generate an XML file with various benchmarks to be plotted on Jenkins.

--- test/xml_benchmark.dart
+++ test/xml_benchmark.dart
@@ -3,10 +3,13 @@ library xml_benchmark;
 import 'package:xml/xml.dart';
 import 'xml_examples.dart';
 
-double benchmark(Function function, [int milliseconds = 2500]) {
+double benchmark(Function function, [int warmup = 5, int milliseconds = 2500]) {
   var count = 0;
   var elapsed = 0;
   var watch = new Stopwatch();
+  while (warmup-- > 0) {
+    function();
+  }
   watch.start();
   while (elapsed < milliseconds) {
     function();
@@ -16,10 +19,37 @@ double benchmark(Function function, [int milliseconds = 2500]) {
   return elapsed / count;
 }
 
-void parsing() {
-  parse(booksXml);
+String characterData() {
+  var string = '''a&bc<def"gehi'jklm>nopqr''';
+  var builder = new XmlBuilder();
+  builder.processing('xml', 'version="1.0"');
+  builder.element('character', nest: () {
+    for (var i = 0; i < 20; i++) {
+      builder.text(string + string + string + string + string + string);
+      builder.element('foo', nest: () {
+        builder.attribute('key', string + string + string + string);
+      });
+    }
+  });
+  return builder.build().toString();
 }
 
+final benchmarks = {
+  'books': booksXml,
+  'shiporder': shiporderXsd,
+  'decoding': characterData(),
+};
+
 void main() {
-  print('Parsing: ${benchmark(parsing)}ms');
+  var builder = new XmlBuilder();
+  builder.processing('xml', 'version="1.0"');
+  builder.element('benchmarks', nest: () {
+    for (var name in benchmarks.keys) {
+      builder.element(name, nest: () {
+        final source = benchmarks[name];
+        builder.text(benchmark(() => parse(source)));
+      });
+    }
+  });
+  print(builder.build().toXmlString(pretty: true));
 }
