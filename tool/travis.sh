45644b4d72ecc93739a0b175a4760365f1666d90
Lukas Renggli <renggli@gmail.com>
2017-05-09 22:53:57 +0200

    Update TravisCI configuration that deprecated content-shell.

--- tool/travis.sh
+++ tool/travis.sh
@@ -5,11 +5,11 @@ set -e
 
 # Verify that the libraries are error free.
 dartanalyzer --fatal-warnings \
-  lib/*.dart \
-  test/*.dart
+    lib/*.dart \
+    test/*.dart
 
 # Verify that all the tests pass.
-dart --checked test/all_tests.dart
+pub run --checked test -p vm test/all_tests.dart
 
 # Verify the coverage of the tests.
 if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
