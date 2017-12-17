91ef05629fffb7212a6002d7b2d4d8f574067925
Lukas Renggli <renggli@gmail.com>
2017-12-17 12:33:49 +0100

    More sensible formatting of XmlAttributeType.

--- lib/xml/utils/attribute_type.dart
+++ lib/xml/utils/attribute_type.dart
@@ -1,4 +1,7 @@
 library xml.utils.attribute_type;
 
 /// Enum of the attribute quote types.
-enum XmlAttributeType { SINGLE_QUOTE, DOUBLE_QUOTE }
+enum XmlAttributeType {
+  SINGLE_QUOTE,
+  DOUBLE_QUOTE,
+}
