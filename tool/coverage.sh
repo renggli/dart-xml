d4e5ab203eda5e9ca7fe4266f4213e05a03de5bf
Lukas Renggli <renggli@gmail.com>
2018-03-10 12:44:01 +0100

    More streamlining of the travis build.

--- tool/coverage.sh
+++ tool/coverage.sh
@@ -3,14 +3,8 @@
 # Fast fail the script on failures.
 set -e
 
-# Verify that the libraries are error free.
-dartanalyzer --fatal-warnings $(find . -name "*.dart")
-
-# Verify that all the tests pass.
-pub run test
-
 # Verify the coverage of the tests.
-if [ "${COVERALLS_TOKEN}" ] && [ "${TRAVIS_DART_VERSION}" = "stable" ]; then
+if [ "${COVERALLS_TOKEN}" ] ; then
   pub global activate dart_coveralls
   pub global run dart_coveralls report \
     --token "${COVERALLS_TOKEN}" \
