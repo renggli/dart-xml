c6520e327499a8296cb76a3fbaa7a644b1181238
Lukas Renggli <renggli@gmail.com>
2018-07-15 20:30:28 +0200

    Split out the productions in separate defintion for reuse in SAX parser.

--- lib/xml/grammar.dart
+++ lib/xml/grammar.dart
@@ -1,21 +1,14 @@
 library xml.grammar;
 
 import 'package:petitparser/petitparser.dart';
+import 'package:xml/xml/production.dart';
 import 'package:xml/xml/utils/attribute_type.dart';
-import 'package:xml/xml/utils/entities.dart';
 import 'package:xml/xml/utils/token.dart';
 
 /// XML grammar definition with [TNode] and [TName].
-abstract class XmlGrammarDefinition<TNode, TName> extends GrammarDefinition {
-  // name patterns
-  static const String _nameStartChars =
-      ':A-Z_a-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF'
-      '\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001\uD7FF'
-      '\uF900-\uFDCF\uFDF0-\uFFFD';
-  static const String _nameChars =
-      '-.0-9\u00B7\u0300-\u036F\u203F-\u2040$_nameStartChars';
+abstract class XmlGrammarDefinition<TNode, TName> extends XmlProductionDefinition {
 
-  // parser callbacks
+  // Callbacks used to build the XML AST.
   TNode createAttribute(TName name, String text, XmlAttributeType type);
   TNode createComment(String text);
   TNode createCDATA(String text);
@@ -26,61 +19,36 @@ abstract class XmlGrammarDefinition<TNode, TName> extends GrammarDefinition {
   TName createQualified(String name);
   TNode createText(String text);
 
-  // productions
-  @override
-  Parser start() => ref(document).end();
+  // Connects the productions and the XML AST callbacks.
 
-  Parser attribute() => ref(qualified)
-      .seq(ref(spaceOptional))
-      .seq(char(XmlToken.equals))
-      .seq(ref(spaceOptional))
-      .seq(ref(attributeValue))
+  @override
+  Parser attribute() => super.attribute()
       .map((each) => createAttribute(each[0] as TName, each[4][0], each[4][1]));
-  Parser attributeValue() =>
-      ref(attributeValueDouble).or(ref(attributeValueSingle));
-  Parser attributeValueDouble() => char(XmlToken.doubleQuote)
-      .seq(new XmlCharacterDataParser(XmlToken.doubleQuote, 0))
-      .seq(char(XmlToken.doubleQuote))
+
+  @override
+  Parser attributeValueDouble() => super.attributeValueDouble()
       .map((each) => [each[1], XmlAttributeType.DOUBLE_QUOTE]);
-  Parser attributeValueSingle() => char(XmlToken.singleQuote)
-      .seq(new XmlCharacterDataParser(XmlToken.singleQuote, 0))
-      .seq(char(XmlToken.singleQuote))
+
+  @override
+  Parser attributeValueSingle() => super.attributeValueSingle()
       .map((each) => [each[1], XmlAttributeType.SINGLE_QUOTE]);
-  Parser attributes() => ref(space).seq(ref(attribute)).pick(1).star();
-  Parser comment() => string(XmlToken.openComment)
-      .seq(any().starLazy(string(XmlToken.closeComment)).flatten())
-      .seq(string(XmlToken.closeComment))
+
+  @override
+  Parser comment() => super.comment()
       .map((each) => createComment(each[1]));
-  Parser cdata() => string(XmlToken.openCDATA)
-      .seq(any().starLazy(string(XmlToken.closeCDATA)).flatten())
-      .seq(string(XmlToken.closeCDATA))
+
+  @override
+  Parser cdata() => super.cdata()
       .map((each) => createCDATA(each[1]));
-  Parser content() => ref(characterData)
-      .or(ref(element))
-      .or(ref(processing))
-      .or(ref(comment))
-      .or(ref(cdata))
-      .star();
-  Parser doctype() => string(XmlToken.openDoctype)
-      .seq(ref(space))
-      .seq(ref(nameToken)
-          .or(ref(attributeValue))
-          .or(any()
-              .starLazy(char(XmlToken.openDoctypeBlock))
-              .seq(char(XmlToken.openDoctypeBlock))
-              .seq(any().starLazy(char(XmlToken.closeDoctypeBlock)))
-              .seq(char(XmlToken.closeDoctypeBlock)))
-          .separatedBy(ref(space))
-          .flatten())
-      .seq(ref(spaceOptional))
-      .seq(char(XmlToken.closeDoctype))
+
+
+  @override
+  Parser doctype() => super.doctype()
       .map((each) => createDoctype(each[2]));
-  Parser document() => ref(misc)
-          .seq(ref(doctype).optional())
-          .seq(ref(misc))
-          .seq(ref(element))
-          .seq(ref(misc))
-          .map((each) {
+
+  @override
+  Parser document() => super.document()
+      .map((each) {
         var nodes = [];
         nodes.addAll(each[0]);
         if (each[1] != null) {
@@ -91,17 +59,10 @@ abstract class XmlGrammarDefinition<TNode, TName> extends GrammarDefinition {
         nodes.addAll(each[4]);
         return createDocument(new List<TNode>.from(nodes));
       });
-  Parser element() => char(XmlToken.openElement)
-          .seq(ref(qualified))
-          .seq(ref(attributes))
-          .seq(ref(spaceOptional))
-          .seq(string(XmlToken.closeEndElement).or(char(XmlToken.closeElement)
-              .seq(ref(content))
-              .seq(string(XmlToken.openEndElement))
-              .seq(ref(qualified))
-              .seq(ref(spaceOptional))
-              .seq(char(XmlToken.closeElement))))
-          .map((list) {
+
+  @override
+  Parser element() => super.element()
+     .map((list) {
         if (list[4] == XmlToken.closeEndElement) {
           return createElement(
               list[1] as TName, new List<TNode>.from(list[2]), []);
@@ -117,24 +78,20 @@ abstract class XmlGrammarDefinition<TNode, TName> extends GrammarDefinition {
           }
         }
       });
-  Parser processing() => string(XmlToken.openProcessing)
-      .seq(ref(nameToken))
-      .seq(ref(space)
-          .seq(any().starLazy(string(XmlToken.closeProcessing)).flatten())
-          .pick(1)
-          .optional(''))
-      .seq(string(XmlToken.closeProcessing))
+
+  @override
+  Parser processing() => super.processing()
       .map((each) => createProcessing(each[1], each[2]));
-  Parser qualified() => ref(nameToken).map(createQualified);
 
-  Parser characterData() =>
-      new XmlCharacterDataParser(XmlToken.openElement, 1).map(createText);
-  Parser misc() => ref(spaceText).or(ref(comment)).or(ref(processing)).star();
-  Parser space() => whitespace().plus();
-  Parser spaceText() => ref(space).flatten().map(createText);
-  Parser spaceOptional() => whitespace().star();
+  @override
+  Parser qualified() => super.qualified()
+      .map(createQualified);
 
-  Parser nameToken() => ref(nameStartChar).seq(ref(nameChar).star()).flatten();
-  Parser nameStartChar() => pattern(_nameStartChars, 'Expected name');
-  Parser nameChar() => pattern(_nameChars);
+  @override
+  Parser characterData() => super.characterData()
+      .map(createText);
+
+  @override
+  Parser spaceText() => super.spaceText()
+      .map(createText);
 }
