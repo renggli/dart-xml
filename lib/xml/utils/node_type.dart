6ce9dcfe2ee43d8fbd8dab14505378700e9f48c2
Dan Field <dnfield@google.com>
2018-11-30 01:56:20 -0800

    format

--- lib/xml/utils/node_type.dart
+++ lib/xml/utils/node_type.dart
@@ -4,22 +4,31 @@ library xml.utils.node_type;
 enum XmlNodeType {
   /// An element start or self-closed tag, e.g. `<item>` or `<item />`.
   ELEMENT,
+
   /// An element closing tag, e.g. `</item>`.
   END_ELEMENT,
+
   /// An attribute, e.g. `id="123"`.
   ATTRIBUTE,
+
   /// Text content within a node.
   TEXT,
+
   /// Raw character data (CDATA), e.g.  `<![CDATA[escaped text]]>`.
   CDATA,
+
   /// A processing instruction, e.g. `<?pi test?>`.
   PROCESSING,
+
   /// A comment, e.g. `<!-- comment -->`.
   COMMENT,
+
   /// A document object.
   DOCUMENT,
+
   /// A document fragment.
   DOCUMENT_FRAGMENT,
+
   /// A document type declaration, e.g. `<!DOCTYPE...>`.
   DOCUMENT_TYPE
 }
@@ -33,16 +42,22 @@ enum XmlNodeType {
 enum XmlPushReaderNodeType {
   /// An element start or self-closed tag, e.g. `<item>` or `<item />`.
   ELEMENT,
+
   /// An element closing tag, e.g. `</item>`.
   END_ELEMENT,
+
   /// Text content within a node.
   TEXT,
+
   /// Raw character data (CDATA), e.g.  `<![CDATA[escaped text]]>`.
   CDATA,
+
   /// A processing instruction, e.g. `<?pi test?>`.
   PROCESSING,
+
   /// A comment, e.g. `<!-- comment -->`.
   COMMENT,
+
   /// A document type declaration, e.g. `<!DOCTYPE...>`.
   DOCUMENT_TYPE
 }
