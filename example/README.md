2d017102dc31c7f4fc3174e407b458f8eac74ca2
Lukas Renggli <renggli@gmail.com>
2019-03-07 08:01:28 +0100

    Add an event based parser example.

--- example/README.md
+++ example/README.md
@@ -3,6 +3,12 @@ Dart XML Examples
 
 This package contains examples to illustrate the use of [Dart XML](https://github.com/renggli/dart-xml). A tutorial and full documentation is contained in the [package description](https://pub.dartlang.org/packages/xml) and [API documentation](https://pub.dartlang.org/documentation/xml/latest/).
 
+### xml_flatten
+
+This example contains a command-line application that flattens an XML documents from the file-system into a list of events that are printed to the console. For example: 
+
+    dart example/xml_flatten.dart example/books.xml
+
 ### xml_pp
 
 This example contains a command-line application that reads XML documents from the file-system and pretty prints the formatted document to the console.
---
+++ example/xml_flatten.dart
@@ -0,0 +1,63 @@
+/// XML flatten.
+library xml.example.xml_flatten;
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:args/args.dart' as args;
+import 'package:xml/xml_events.dart' as xml_events;
+
+final args.ArgParser argumentParser = args.ArgParser()
+  ..addFlag(
+    'normalize',
+    abbr: 'n',
+    help: 'Normalizes the output stream.',
+  )
+  ..addFlag(
+    'text',
+    abbr: 't',
+    help: 'Only display text events',
+  );
+
+void printUsage() {
+  stdout.writeln('Usage: xml_flatten [options] {files}');
+  stdout.writeln();
+  stdout.writeln(argumentParser.usage);
+  exit(1);
+}
+
+void main(List<String> arguments) async {
+  final files = <File>[];
+  final results = argumentParser.parse(arguments);
+  final normalize = results['normalize'];
+  final text = results['text'];
+
+  for (var argument in results.rest) {
+    final file = File(argument);
+    if (file.existsSync()) {
+      files.add(file);
+    } else {
+      stderr.writeln('File not found: $file');
+      exit(2);
+    }
+  }
+  if (files.isEmpty) {
+    printUsage();
+  }
+
+  for (var file in files) {
+    var stream = file
+        .openRead()
+        .transform(utf8.decoder)
+        .transform(const xml_events.XmlEventDecoder());
+    if (normalize) {
+      stream = stream.transform(const xml_events.XmlNormalizer());
+    }
+    var flatStream = stream.expand((events) => events);
+    if (text) {
+      flatStream =
+          flatStream.where((event) => event is xml_events.XmlTextEvent);
+    }
+    await flatStream.forEach(print);
+  }
+}
