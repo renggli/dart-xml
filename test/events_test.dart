c65aaec494cd8282e34be4a3fb5ca441cedc2ac5
Lukas Renggli <renggli@gmail.com>
2019-04-22 18:15:58 +0200

    Update all the linter rules.

--- test/events_test.dart
+++ test/events_test.dart
@@ -188,7 +188,7 @@ void main() {
             const XmlEventDecoder().startChunkedConversion(accumulator);
         expect(() => converter.addSlice('a<', 0, 2, true),
             throwsA(isXmlParserException));
-        converter.close();
+        expect(() => converter.close(), throwsA(isXmlParserException));
       });
     });
   });
