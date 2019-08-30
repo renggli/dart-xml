f3a5262e28a2545aeac700adf407c435ff64e0c1
Lukas Renggli <renggli@gmail.com>
2019-08-30 07:28:20 +0200

    Add an example calling a HTTP XML API service.

--- example/README.md
+++ example/README.md
@@ -3,6 +3,12 @@ Dart XML Examples
 
 This package contains examples to illustrate the use of [Dart XML](https://github.com/renggli/dart-xml). A tutorial and full documentation is contained in the [package description](https://pub.dartlang.org/packages/xml) and [API documentation](https://pub.dartlang.org/documentation/xml/latest/).
 
+### ip_lookup
+
+This example performs an API call to [ip-api.com](http://ip-api.com/) to search for IP and domain meta-data. If no query is provided the current IP address will be used. Various options can be changed over the command line arguments.
+
+    dart example/ip.dart example/books.xml
+
 ### xml_flatten
 
 This example contains a command-line application that flattens an XML documents from the file-system into a list of events that are printed to the console. For example: 
---
+++ example/ip_api.dart
@@ -0,0 +1,127 @@
+/// IP Geolocation API using ip-api.com.
+library xml.example.ip_api;
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:args/args.dart' as args;
+import 'package:xml/xml.dart' as xml;
+import 'package:xml/xml_events.dart' as xml_events;
+
+final HttpClient httpClient = HttpClient();
+
+final args.ArgParser argumentParser = args.ArgParser()
+  ..addFlag(
+    'incremental',
+    abbr: 'i',
+    defaultsTo: true,
+    help: 'Incrementally parses and prints the response',
+  )
+  ..addMultiOption('fields',
+      abbr: 'f',
+      defaultsTo: ['status', 'message', 'query', 'country', 'city'],
+      allowed: [
+        'as',
+        'asname',
+        'city',
+        'continent',
+        'continentCode',
+        'country',
+        'countryCode',
+        'currency',
+        'district',
+        'isp',
+        'lat',
+        'lon',
+        'message',
+        'mobile',
+        'org',
+        'proxy',
+        'query',
+        'region',
+        'regionName',
+        'reverse',
+        'status',
+        'timezone',
+        'zip',
+      ],
+      help: 'Fields to be returned')
+  ..addOption(
+    'lang',
+    abbr: 'l',
+    defaultsTo: 'en',
+    allowed: ['en', 'de', 'es', 'pt-BR', 'fr', 'ja', 'zh-CN', 'ru'],
+    help: 'Localizes city, region and country names',
+  )
+  ..addFlag(
+    'help',
+    abbr: 'h',
+    defaultsTo: false,
+    help: 'Displays the help text',
+  );
+
+void printUsage() {
+  stdout.writeln('Usage: ip_lookup -s [query]');
+  stdout.writeln();
+  stdout.writeln(argumentParser.usage);
+  exit(1);
+}
+
+Future<void> lookupIp(args.ArgResults results, [String query = '']) async {
+  final bool incremental = results['incremental'];
+  final List<String> fields = results['fields'];
+  final String lang = results['lang'];
+
+  // Build the query URL, perform the request, and convert response to UTF-8.
+  final url = Uri.parse(
+      'http://ip-api.com/xml/$query?fields=${fields.join(',')}&lang=$lang');
+  final request = await httpClient.getUrl(url);
+  final response = await request.close();
+  final stream = response.transform(utf8.decoder);
+
+  // Typically you would only implement one of the following two approaches,
+  // but for demonstration sake we demonstrate both in this example:
+  if (incremental) {
+    /// Decode the input stream, normalize it and serialize events, then extract
+    /// the information to be printed.
+    var name = '';
+    await stream
+        .transform(const xml_events.XmlEventDecoder())
+        .transform(const xml_events.XmlNormalizer())
+        .expand((events) => events)
+        .forEach((event) {
+      if (event is xml_events.XmlStartElementEvent) {
+        name = event.name;
+      } else if (event is xml_events.XmlCDATAEvent) {
+        stdout.writeln('$name: ${event.text}');
+      }
+    });
+  } else {
+    // Wait until we have the full response body, then parse the input to a
+    // XML DOM tree and extract the information to be printed.
+    final input = await stream.join();
+    final document = xml.parse(input);
+    for (final element in document.rootElement.children) {
+      if (element is xml.XmlElement) {
+        stdout.writeln('${element.name}: ${element.text}');
+      }
+    }
+  }
+}
+
+Future<void> main(List<String> arguments) async {
+  final results = argumentParser.parse(arguments);
+
+  if (results['help']) {
+    printUsage();
+  }
+
+  if (results.rest.isEmpty) {
+    await lookupIp(results);
+  } else {
+    for (final query in results.rest) {
+      await lookupIp(results, query);
+      stdout.writeln();
+    }
+  }
+}
