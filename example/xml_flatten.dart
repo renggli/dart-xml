d4e51de6df8bb1380d9c08ebb7db22e98ad265eb
Lukas Renggli <renggli@gmail.com>
2023-01-14 17:25:24 +0100

    Make the `xml_flatten` example more powerful/useful.

--- example/xml_flatten.dart
+++ example/xml_flatten.dart
@@ -5,16 +5,23 @@ import 'dart:io';
 import 'package:args/args.dart' as args;
 import 'package:xml/xml_events.dart';
 
+final formatRegExp = RegExp(r'%(.)');
+
 final args.ArgParser argumentParser = args.ArgParser()
+  ..addOption(
+    'format',
+    abbr: 'f',
+    help: 'Format string of each event:\n'
+        '\t%s Event string\n'
+        '\t%t Node type\n'
+        '\t%p Position',
+    defaultsTo: '%t: %s',
+  )
   ..addFlag(
     'normalize',
     abbr: 'n',
     help: 'Normalize the output stream.',
-  )
-  ..addFlag(
-    'text',
-    abbr: 't',
-    help: 'Only display text events.',
+    defaultsTo: true,
   );
 
 void printUsage() {
@@ -24,11 +31,25 @@ void printUsage() {
   exit(1);
 }
 
+String formatEvent(XmlEvent event, String format) =>
+    format.splitMapJoin(formatRegExp, onMatch: (match) {
+      switch (match.group(1)) {
+        case 's':
+          return event.toString();
+        case 't':
+          return event.nodeType.toString();
+        case 'p':
+          return event.start.toString();
+        default:
+          return match.group(1)!;
+      }
+    });
+
 Future<void> main(List<String> arguments) async {
   final files = <File>[];
   final results = argumentParser.parse(arguments);
+  final format = results['format'] as String;
   final normalize = results['normalize'] as bool;
-  final text = results['text'] as bool;
 
   for (final argument in results.rest) {
     final file = File(argument);
@@ -44,14 +65,14 @@ Future<void> main(List<String> arguments) async {
   }
 
   for (final file in files) {
-    var stream = file.openRead().transform(utf8.decoder).toXmlEvents();
+    var stream =
+        file.openRead().transform(utf8.decoder).toXmlEvents(withLocation: true);
     if (normalize) {
       stream = stream.normalizeEvents();
     }
-    var flatStream = stream.flatten();
-    if (text) {
-      flatStream = flatStream.where((event) => event is XmlTextEvent);
-    }
-    await flatStream.forEach(stdout.writeln);
+    await stream
+        .flatten()
+        .map((event) => formatEvent(event, format))
+        .forEach(stdout.writeln);
   }
 }
